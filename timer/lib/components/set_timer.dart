826e8358dd698ebae8d59e4d10f462c5590f4e28
lateofrederick <lateofrederick@gmail.com>
2021-01-25 21:30:41 +0000

    implemented set timer class

---
+++ timer/lib/components/set_timer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SetTimer extends StatefulWidget {
+  @override
+  _SetTimerState createState() => _SetTimerState();
+}
+
+class _SetTimerState extends State<SetTimer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
