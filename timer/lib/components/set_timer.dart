f76eb57c03fc15ca47ee4f38028cef7a4570fde0
lateofrederick <30894559+lateofrederick@users.noreply.github.com>
2021-04-23 06:42:02 -0700

    implemented the screen to set a timer

--- timer/lib/components/set_timer.dart
+++ timer/lib/components/set_timer.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:timer/timer.dart';
 
 class SetTimer extends StatefulWidget {
   @override
@@ -7,10 +8,56 @@ class SetTimer extends StatefulWidget {
 }
 
 class _SetTimerState extends State<SetTimer> {
+  Duration _time;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child: Center(
+              child: Column(
+                children: [
+                  CupertinoTimerPicker(
+                    onTimerDurationChanged: (val) {
+                      setState(() {
+                        _time = val;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            top: MediaQuery.of(context).size.height * 0.35,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          ),
+          Positioned(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50.0,
+              child: ElevatedButton(
+                child: Text("start"),
+                onPressed: () {
+                  print("===============");
+                  print(_time);
+                  print("===============");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Timer(_time))
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    textStyle: TextStyle(fontSize: 28.0)),
+              ),
+            ),
+            bottom: 0,
+          )
+        ],
+      ),
     );
   }
 }
